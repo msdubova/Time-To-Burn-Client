78b43b7da67661df612f87be852bf6e543ef41ed
CharlesMartinUK <charles_martin@hotmail.co.uk>
2023-09-21 14:05:44 +0100

    Display results as separate component

---
+++ client/src/components/display_results.tsx
@@ -0,0 +1,73 @@
+
+
+export type DisplayResultsProp = {json:any}
+
+export const DisplayResults : React.FC<DisplayResultsProp> =({json}) => {
+
+	//console.log(json)
+
+	if(json === null) {
+		return (<>
+		
+		</>
+		)
+	}
+	
+
+	
+	return (
+		<>
+			<div><h2>Food Details:</h2>
+		
+		<table className="resultsTable">
+			<thead>
+				<tr>
+					<th>Name</th><th>Calories</th><th>serving size</th><th>Fat Total</th><th>Protein (grams)</th>
+					<th>Potassium (mg)</th><th>Cholesterol</th><th>Total Carbohyrates</th>
+				</tr>
+			</thead>
+			<tbody>
+						
+			
+		{json.items.map((f:any,i:number) => {
+			return <tr key={i}><td>{f.name}</td><td>{f.serving_size_g}</td><td>{f.calories}</td><td>{f.protein_g}</td>
+			<td>{f.sodium_mg}</td><td>{f.potassium_mg}</td><td>{f.cholesterol_mg}</td><td>{f.carbohydrates_total_g}</td></tr>	
+		})
+		}
+		</tbody>
+		</table>
+		</div>
+		
+		<div>
+			<h2>Total Calories: {json.total_calories}</h2>
+		</div>
+			
+			<div><h2>Exercise Details:</h2>
+		<table className="resultsTable">
+			<thead>
+				<tr>
+					<th>Exercise</th><th>Hours</th><th>Minutes</th>
+				</tr>
+			</thead>
+			<tbody>
+			{
+			json.exercises.map((e:any,i:number) => {
+				return <tr key={i}><td>{e.name}</td><td>{e.duration_hr}</td><td>{e.duration_minutes}</td></tr>
+				
+			})
+			
+			}
+			</tbody>
+		</table>
+		
+		</div>
+			
+			
+		</>
+	
+	)
+
+
+}
+
+
--- client/src/components/get_data.tsx
+++ client/src/components/get_data.tsx
@@ -1,5 +1,7 @@
 import {useEffect, useState} from "react"
 
+import {DisplayResults} from './display_results'
+
 import URL from "../util/url"
 
 
@@ -121,76 +123,17 @@ const TimeToBurnAPI = () => {
 	if(textInput.length > 2) buttonDisabled = false
 
 
-	if(json == null) {
+
 
 		//uses scan so findtext can find each field
-		return (
-			<>
-	<div><input type={"text"} size={60} value={textInput} onChange={(e) => setTextInput(e.target.value) } /> </div>
-	<div><button disabled={buttonDisabled} onClick={calculateSubmit}>Calculate</button> </div>
-			</>
-			
-		)
-	}
-	else {
-		//console.log(json.items)
-		return (
+	return (
 		<>
-		
-		 <>
-	<div><input type={"text"} size={60} value={textInput} onChange={(e) => setTextInput(e.target.value) } /> </div>
+			<div><input type={"text"} size={60} value={textInput} onChange={(e) => setTextInput(e.target.value) } /> </div>
 			<div><button disabled={buttonDisabled} onClick={calculateSubmit}>Calculate</button> </div>
-        </>
-		
-		
-		<div><h2>Food Details:</h2>
-		
-		<table className="resultsTable">
-			<thead>
-				<tr>
-					<th>Name</th><th>Calories</th><th>serving size</th><th>Fat Total</th><th>Protein (grams)</th>
-					<th>Potassium (mg)</th><th>Cholesterol</th><th>Total Carbohyrates</th>
-				</tr>
-			</thead>
-			<tbody>
-						
 			
-		{json.items.map((f,i) => {
-			return <tr key={i}><td>{f.name}</td><td>{f.serving_size_g}</td><td>{f.calories}</td><td>{f.protein_g}</td>
-			<td>{f.sodium_mg}</td><td>{f.potassium_mg}</td><td>{f.cholesterol_mg}</td><td>{f.carbohydrates_total_g}</td></tr>	
-		})
-		}
-		</tbody>
-		</table>
-		</div>
-		<div>
-			<h2>Total Calories: {json.total_calories}</h2>
-		</div>
-		
-		<div><h2>Exercise Details:</h2>
-		<table className="resultsTable">
-			<thead>
-				<tr>
-					<th>Exercise</th><th>Hours</th><th>Minutes</th>
-				</tr>
-			</thead>
-			<tbody>
-			{
-			json.exercises.map((e,i) => {
-				return <tr key={i}><td>{e.name}</td><td>{e.duration_hr}</td><td>{e.duration_minutes}</td></tr>
-				
-			})
-			
-			}
-			</tbody>
-		</table>
-		
-		</div>
-		
-		
+			<DisplayResults  json={json}/>
 		</>
-		)
-	}
+	)
 	
 }
 
