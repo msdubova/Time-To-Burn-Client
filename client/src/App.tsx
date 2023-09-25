bf0e8f9801c2b2dae4c4d2f72376823dc9647ebd
CharlesMartinUK <charles_martin@hotmail.co.uk>
2023-09-25 17:07:14 +0100

    Changes to main page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,12 +10,13 @@ import TimeToBurnAPI from "./components/get_data"
 import './App.css';
 
 function App() { 
-	
+	// <img src="animated-fire-image-0005.gif" alt="fire" width={40} height={40} />
   return (
     <div className="App">
       <header >
         <h1><img src="pngegg.png" width={40} height={40} alt="Stop watch"/>Time To Burn
-		<img src="animated-fire-image-0005.gif" alt="fire" width={40} height={40} /></h1>
+    <img src="pngegg.png" width={40} height={40} alt="Stop watch"/>
+    </h1>
         Learn how much exercise is needed to burn those calories<br/>
         Usage:100g carrots  2 apples
 		</header>
