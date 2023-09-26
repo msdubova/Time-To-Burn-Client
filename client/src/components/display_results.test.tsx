34977074dbec577632aabc4da7a65b57400f5630
CharlesMartinUK <charles_martin@hotmail.co.uk>
2023-09-26 21:10:34 +0100

    Tests for display results

--- client/src/components/display_results.test.tsx
+++ client/src/components/display_results.test.tsx
@@ -8,10 +8,91 @@ import URL from "../util/url"
 
 import {DisplayResults} from "./display_results";
 
+const PARAM_TEXT = "2 apples 400g chicken"
+
 
 const server = setupServer(
-  rest.get( URL , (req, res, ctx) => {
-    return res(ctx.json({ name: "Luke Skywalker", height:"172", birth_year:"19BBY", eye_colour:"blue" }))
+  rest.get( URL + encodeURIComponent(PARAM_TEXT) , (req, res, ctx) => {
+    return res(ctx.json({ 
+      "items": [
+        {
+          "name": "apples",
+          "calories": 194.5,
+          "serving_size_g": 364,
+          "fat_total_g": 0.6,
+          "fat_saturated_g": 0.1,
+          "protein_g": 0.9,
+          "sodium_mg": 3,
+          "potassium_mg": 40,
+          "cholesterol_mg": 0,
+          "carbohydrates_total_g": 50.1,
+          "fiber_g": 8.6,
+          "sugar_g": 37.7
+        },
+        {
+          "name": "chicken",
+          "calories": 445.3,
+          "serving_size_g": 200,
+          "fat_total_g": 25.8,
+          "fat_saturated_g": 7.3,
+          "protein_g": 47.5,
+          "sodium_mg": 144,
+          "potassium_mg": 358,
+          "cholesterol_mg": 184,
+          "carbohydrates_total_g": 0.1,
+          "fiber_g": 0,
+          "sugar_g": 0
+        }
+      ],
+      "total_calories": 639.8,
+      "exercises": [
+        {
+          "name": "Running",
+          "calsPerHour": 1074,
+          "time_to_burn_total_cals": {
+            "seconds": 2144.58,
+            "minutes": 35.74,
+            "hours": 0.6
+          }
+        },
+        {
+          "name": "Jump Rope",
+          "calsPerHour": 1070,
+          "time_to_burn_total_cals": {
+            "seconds": 2152.6,
+            "minutes": 35.88,
+            "hours": 0.6
+          }
+        },
+        {
+          "name": "Taekwondo",
+          "calsPerHour": 937,
+          "time_to_burn_total_cals": {
+            "seconds": 2458.14,
+            "minutes": 40.97,
+            "hours": 0.68
+          }
+        },
+        {
+          "name": "Swimming",
+          "calsPerHour": 892,
+          "time_to_burn_total_cals": {
+            "seconds": 2582.15,
+            "minutes": 43.04,
+            "hours": 0.72
+          }
+        },
+        {
+          "name": "Running up Stairs",
+          "calsPerHour": 819,
+          "time_to_burn_total_cals": {
+            "seconds": 2812.31,
+            "minutes": 46.87,
+            "hours": 0.78
+          }
+        }
+      ]
+    }))
   })
 )
 
@@ -22,31 +103,29 @@ afterAll(() => server.close())
 
 describe("<DisplayResults>",  () => {
 	
-    test("Is displayed ?",  () => {
-        //render(<DisplayResults />);
+    test("Is displaying at all ?",  async () => {
+        render(<DisplayResults text={PARAM_TEXT}/>);
   
-        //expect( screen.getByText("Calculate")).toBeInTheDocument()
+        expect( await screen.findByText("Food Details:")).toBeInTheDocument() 
     });
   
-  /*
-  test("on server error 500,  correct error message", async () => {
-    server.use(
-      rest.get(URL , (req, res, ctx) => {
-        return res(ctx.status(500));
-      })
-    );
-    render(<App />);
-    expect(await screen.findByText(ERROR_500)).toBeInTheDocument();
+
+    test("Is showing food ?",  async () => {
+      render(<DisplayResults text={PARAM_TEXT}/>);
+
+      expect( await screen.findByText("apples")).toBeInTheDocument() 
+      expect( await screen.findByText("chicken")).toBeInTheDocument() 
   });
 
-  test("on server error 418,  correct error message", async () => {
-    server.use(
-      rest.get(URL, (req, res, ctx) => {
-        return res(ctx.status(418));
-      })
-    );
-    render(<App />);
-    expect(await screen.findByText(ERROR_418)).toBeInTheDocument();
-  });
-  */
+  test("Is showing exercise title and details?",  async () => {
+    render(<DisplayResults text={PARAM_TEXT}/>);
+
+    expect( await screen.findByText("Exercise Details:")).toBeInTheDocument() 
+    expect( await screen.findByText("Running")).toBeInTheDocument() 
+    expect( await screen.findByText("Jump Rope")).toBeInTheDocument() 
+    expect( await screen.findByText("Taekwondo")).toBeInTheDocument() 
+    expect( await screen.findByText("Swimming")).toBeInTheDocument() 
+    expect( await screen.findByText("Running up Stairs")).toBeInTheDocument() 
+});
+
 })
