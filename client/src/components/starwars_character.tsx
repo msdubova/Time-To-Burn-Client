3153e83394e8ec771696ed30dc5473fb84562eb7
CharlesMartinUK <charles_martin@hotmail.co.uk>
2023-09-27 15:32:28 +0100

    Deleted unneeded file

--- client/src/components/starwars_character.tsx
+++
@@ -1,5 +0,0 @@
-export type Character = { name: string, birth_year:string, mass:string };
-
-export const StarWarsCharacter: React.FC<Character> = ({ name, birth_year, mass }) => (
-  <div>Name:{name} Birth year:{birth_year} Mass:{mass}</div>
-);
